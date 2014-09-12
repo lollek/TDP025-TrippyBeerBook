8b1798bde1a04cbe75f15c108541b41b16d68081
Olle K <olle+git@iix.se>
2014-09-12 19:24:02 +0200

    Remove Home and Title from ActionBar

--- app/src/main/java/iix/se/cron/MainActivity.java
+++ app/src/main/java/iix/se/cron/MainActivity.java
@@ -5,9 +5,6 @@ import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
 
@@ -19,9 +16,11 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up the action bar.
+        // Set up the action bar. Add tabs, hide home and title
         final ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
 
         // Create the adapter that will return a fragment for each of the tabs
         mTagFragmentPagerAdapter = new TabFragmentPagerAdapter(this, getFragmentManager());
@@ -47,26 +46,6 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
                             .setText(mTagFragmentPagerAdapter.getPageTitle(i))
                             .setTabListener(this));
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
