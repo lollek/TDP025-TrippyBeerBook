79b8a097ca953ae633c0d3b3501037b88e1715e2
Olle K <olle+git@iix.se>
2014-10-09 18:25:40 +0200

    Move button - added some colours

--- app/src/main/java/iix/se/trippybeerbook/BeerDetailActivity.java
+++ app/src/main/java/iix/se/trippybeerbook/BeerDetailActivity.java
@@ -1,13 +1,11 @@
 package iix.se.trippybeerbook;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
