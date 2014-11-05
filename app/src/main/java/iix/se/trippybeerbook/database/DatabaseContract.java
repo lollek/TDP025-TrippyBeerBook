bd825c053cd60788346bac5d5dd605cfaa5612c2
Olle K <olle+git@iix.se>
2014-11-05 11:39:36 +0100

    Rename column to BeerColumns

--- app/src/main/java/iix/se/trippybeerbook/database/DatabaseContract.java
+++ app/src/main/java/iix/se/trippybeerbook/database/DatabaseContract.java
@@ -4,17 +4,17 @@ import android.provider.BaseColumns;
 
 public class DatabaseContract {
     public static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + TaskReader.TABLE_NAME + " (" +
-                    TaskReader._ID + " INTEGER PRIMARY KEY," +
-                    TaskReader.BEER_NAME  + " TEXT," +
-                    TaskReader.BREWERY    + " TEXT," +
-                    TaskReader.BEER_TYPE  + " TEXT," +
-                    TaskReader.COUNTRY    + " TEXT," +
-                    TaskReader.PERCENTAGE + " TEXT)";
+            "CREATE TABLE " + BeerColumns.TABLE_NAME + " (" +
+                    BeerColumns._ID + " INTEGER PRIMARY KEY," +
+                    BeerColumns.BEER_NAME  + " TEXT," +
+                    BeerColumns.BREWERY    + " TEXT," +
+                    BeerColumns.BEER_TYPE  + " TEXT," +
+                    BeerColumns.COUNTRY    + " TEXT," +
+                    BeerColumns.PERCENTAGE + " TEXT)";
     public static final String SQL_DROP_ENTRIES =
-            "DROP TABLE IF EXISTS " + TaskReader.TABLE_NAME;
+            "DROP TABLE IF EXISTS " + BeerColumns.TABLE_NAME;
 
-    public static abstract class TaskReader implements BaseColumns {
+    public static abstract class BeerColumns implements BaseColumns {
         public static final String TABLE_NAME = "Beers";
         public static final String BEER_NAME = "name";
         public static final String BREWERY = "brewery";
