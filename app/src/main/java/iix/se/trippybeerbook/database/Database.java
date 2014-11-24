9e6d72df8e6f029f698a317b67fa162b12e39b31
Olle K <olle+git@iix.se>
2014-11-24 16:20:53 +0000

    Finally done with list item visuals

--- app/src/main/java/iix/se/trippybeerbook/database/Database.java
+++ app/src/main/java/iix/se/trippybeerbook/database/Database.java
@@ -3,11 +3,6 @@ package iix.se.trippybeerbook.database;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Color;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +13,7 @@ import java.util.List;
 
 public class Database {
     private DatabaseHelper mHelper;
-    private ArrayAdapter<Beer> mAdapter;
+    private BeerArrayAdapter mAdapter;
     private List<Beer> mList;
 
     public Database(Context context) {
@@ -46,31 +41,10 @@ public class Database {
         return mList;
     }
 
-    public ArrayAdapter<Beer> getAdapter(Activity activity) {
+    public BeerArrayAdapter getAdapter(Activity activity) {
         if (mAdapter == null) {
             final List<Beer> list = getList();
-            mAdapter = new ArrayAdapter<Beer>(activity, android.R.layout.simple_list_item_2,
-                    android.R.id.text1, list) {
-                @Override
-                public View getView(int position, View convertView, ViewGroup parent) {
-                    final View view = super.getView(position, convertView, parent);
-                    final TextView text1 = (TextView)view.findViewById(android.R.id.text1);
-                    final TextView text2 = (TextView)view.findViewById(android.R.id.text2);
-                    final Beer item = list.get(position);
-
-                    text1.setText(item.mName);
-                    text2.setText(item.mBrewery);
-
-                    switch((int)Float.parseFloat(item.mStars)) {
-                        case 1: text2.setBackgroundColor(Color.rgb(255,136,0)); break;
-                        case 2: text2.setBackgroundColor(Color.rgb(255,187,51)); break;
-                        case 3: text2.setBackgroundColor(Color.YELLOW); break;
-                        case 4: text2.setBackgroundColor(Color.rgb(153,204,0)); break;
-                        case 5: text2.setBackgroundColor(Color.rgb(102,153,0)); break;
-                    }
-                    return view;
-                }
-            };
+            mAdapter = new BeerArrayAdapter(activity, list);
         }
         return mAdapter;
     }
